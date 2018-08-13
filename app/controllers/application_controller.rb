f82719045483ec38d6fa2e86b7b170a3427daf27
mgoldfi1 <mgoldfi1@gmail.com>
2018-08-13 15:17:11 -0700

    new user form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,4 @@
 require './config/environment'
-
 class ApplicationController < Sinatra::Base
 
   configure do
@@ -9,18 +8,20 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "secret"
   end
 
-  get "/" do
-    erb :welcome
-  end
-
   post "/login" do
     user = User.find_by(username: params[:username])
       if user && user.authenticate(params[:password])
         session[:user_id] = user.id
-        redirect to "/user/reviews"
+        redirect to "/users/#{user.id}"
       else
         redirect to "/"
       end
     end
+
+  get "/" do
+    erb :welcome
+  end
+
+
 
 end
