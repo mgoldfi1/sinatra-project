64e6a794fcbf5e4eefa62b3c8fe30fbdd33bce8c
mgoldfi1 <mgoldfi1@gmail.com>
2018-08-13 14:30:49 -0700

    login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,14 @@ class ApplicationController < Sinatra::Base
     erb :welcome
   end
 
+  post "/login" do
+    user = User.find_by(username: params[:username])
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect to "/user/reviews"
+      else
+        redirect to "/"
+      end
+    end
+
 end
