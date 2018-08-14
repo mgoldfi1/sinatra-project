5f2567477f72cf312465e365b7a566f231855956
mgoldfi1 <mgoldfi1@gmail.com>
2018-08-14 07:43:17 -0700

    edit and delete page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -30,21 +30,45 @@ class PostsController < ApplicationController
   # GET: /posts/5
   get "/posts/:id" do
     @post = Post.find_by_id(params[:id])
+    @session = session
     erb :"/posts/show.html"
   end
 
   # GET: /posts/5/edit
   get "/posts/:id/edit" do
+    @post = Post.find_by_id(params[:id])
+    if session[:user_id] == @post.user.id
     erb :"/posts/edit.html"
+    else
+      redirect "/posts"
+    end
   end
 
-  # PATCH: /posts/5
-  patch "/posts/:id" do
-    redirect "/posts/:id"
+  post "/posts/:id/edit" do
+    post = Post.find_by_id(params[:id])
+    if !params[:content].empty? && !params[:user_rating].empty?
+    post.update(content: params[:content])
+    post.update(user_rating: params[:user_rating])
+    post.save
+    redirect "/posts/#{params[:id]}"
+    else
+      redirect "/posts/#{params[:id]}/edit"
+    end
   end
+
 
   # DELETE: /posts/5/delete
-  delete "/posts/:id/delete" do
-    redirect "/posts"
+  get "/posts/:id/delete" do
+    post = Post.find_by_id(params[:id])
+    if session[:user_id] == post.user.id
+      post.delete
+      redirect "/users/#{session[:user_id]}"
+    else
+    redirect "/users/#{session[:user_id]}"
+    end
   end
-end
+
+
+
+
+  end
